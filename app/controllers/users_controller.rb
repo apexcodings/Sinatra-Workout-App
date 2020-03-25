d3ee88efc7e674bf005a03307fb32b45465ba793
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-03-25 14:54:22 -0400

    setting up workout routes in controller so you can add one and then see it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
   get '/users' do 
+    if Helpers.is_logged_in?(session)
     @users = User.all 
+    else 
+      redirect to '/'
+    end 
 
     erb :'users/index'
   end
@@ -46,7 +50,7 @@ class UsersController < ApplicationController
   end
 
   get '/users/:id' do
-    if User.find_by(id: params[:id])
+    if Helpers.is_logged_in?(session) && User.find_by(id: params[:id])
     @user = User.find_by(id: params[:id])
     else 
       redirect to '/'
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,16 @@
 class WorkoutsController < ApplicationController
 
+  get '/workouts/new' do 
+
+
+    erb :'workouts/new'
+  end
+
+  post '/workouts' do 
+    workout = Workout.create(params)
+    user = Helpers.current_user(session)
+    workout.user = user 
+    redirect to "/users/#{user.id}"
+  end
 
 end
