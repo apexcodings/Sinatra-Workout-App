3f68cdedd56c4a5b1a7f7d4d95fa0f258c059f12
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-03-30 11:54:06 -0400

    fixing update profile session by adjusting validations to be only on: :create

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   has_many :workouts
   has_many :equipments, :through => :workouts
 
-  validates :username, presence: true, uniqueness: true 
-  validates :email, presence: true, uniqueness: true 
-  validates :password_digest, presence: true
+  validates :username, presence: true, uniqueness: true, on: :create
+  validates :email, presence: true, uniqueness: true , on: :create 
+  validates :password_digest, presence: true, on: :create 
 
 
   has_secure_password
