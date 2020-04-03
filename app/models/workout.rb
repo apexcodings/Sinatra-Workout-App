dfc470dbefef311be523f6bf287a1ee1bca06ba0
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-04-03 13:10:19 -0400

    adding validations for workout class

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,4 +3,11 @@ class Workout < ActiveRecord::Base
   belongs_to :user 
   has_many :equipments 
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :instructions, presence: true 
+  validates :workout_time, presence: true
+  validates :experience_needed, presence: true
+  validates :equipment_needed, presence: true
+
 end
